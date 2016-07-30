911fbaf24cd7a619ff92e54e8147c5f3f18da911
velocic <velocicdev@gmail.com>
2016-07-29 20:48:21 -0400

    Still working on my rotation. Math a bit off, and mouse camera controls are erratic

--- 15-camera-control-part-2/include/camera.h
+++ 15-camera-control-part-2/include/camera.h
@@ -4,6 +4,7 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/quaternion.hpp>
+#include <glm/gtx/rotate_vector.hpp>
 
 #include <controls.h>
 #include <utilities.h>
@@ -15,21 +16,23 @@ class Camera
     private:
         glm::mat4 viewMatrix;
         glm::vec3 cameraWorldPosition = glm::vec3(0, 0, 0);
-        glm::vec3 cameraDirection = glm::vec3(0, 0, -1);
-        glm::quat cameraRotation;
-        double movementSpeed = .1;
+        glm::vec3 forwardVector = glm::vec3(0, 0, -1);
+        // glm::quat cameraRotation;
+        float keyboardMovementSpeed = .1;
+        float mouseMovementSpeedDampingFactor = .05;
         unsigned int mousePositionX;
         unsigned int mousePositionY;
         float horizontalAngle;
         float verticalAngle;
 
-        void calculateRotation();
+        void initCameraRotationAngles();
+        void updateCameraRotationAngles(const std::pair<int, int>& mouseDelta);
 
     public:
         Camera(glm::mat4 defaultViewMatrix);
 
         void update(const Controls& input);
-        inline glm::mat4 getViewMatrix() {return viewMatrix;}
+        inline glm::mat4 getViewMatrix() const {return viewMatrix;}
 };
 
 #endif
--- 15-camera-control-part-2/src/camera.cpp
+++ 15-camera-control-part-2/src/camera.cpp
@@ -3,14 +3,15 @@
 Camera::Camera(glm::mat4 defaultViewMatrix) :
     viewMatrix(defaultViewMatrix)
 {
+    initCameraRotationAngles();
 }
 
-void Camera::calculateRotation()
+void Camera::initCameraRotationAngles()
 {
-    auto target = cameraDirection + cameraWorldPosition;
+    auto target = glm::normalize(forwardVector + cameraWorldPosition);
 
-    //Vector parallel to the xz plane
-    auto horizontalTarget = glm::vec3(target.x, 0.0, target.z);
+    //Direction vector parallel to the xz plane
+    auto horizontalTarget = glm::normalize(glm::vec3(target.x, 0.0, target.z));
 
     //Calculate angle on 2D unit circle to determine our horizontal facing
     if (horizontalTarget.z >= 0.0f) {
@@ -30,40 +31,55 @@ void Camera::calculateRotation()
             horizontalAngle = 90.0f + Utilities::Math::radiansToDegrees(asin(-horizontalTarget.z));
         }
     }
+
+    //Vertical angle is limited from -90 to +90 on the yz plane so we can't look up/down into a full circle.
+    verticalAngle = -Utilities::Math::radiansToDegrees(asin(target.y));
+}
+
+void Camera::updateCameraRotationAngles(const std::pair<int, int>& mouseDelta)
+{
+    horizontalAngle += mouseDelta.first * mouseMovementSpeedDampingFactor;
+    verticalAngle += mouseDelta.second * mouseMovementSpeedDampingFactor;
 }
 
 void Camera::update(const Controls& input)
 {
     if (input.isKeyPressed(ControlKeys::Forward)) {
-        cameraWorldPosition.z -= movementSpeed;
+        cameraWorldPosition.z -= keyboardMovementSpeed;
     }
 
     if (input.isKeyPressed(ControlKeys::Backward)) {
-        cameraWorldPosition.z += movementSpeed;
+        cameraWorldPosition.z += keyboardMovementSpeed;
     }
 
     if (input.isKeyPressed(ControlKeys::Left)) {
-        cameraWorldPosition.x -= movementSpeed;
+        cameraWorldPosition.x -= keyboardMovementSpeed;
     }
 
     if (input.isKeyPressed(ControlKeys::Right)) {
-        cameraWorldPosition.x += movementSpeed;
+        cameraWorldPosition.x += keyboardMovementSpeed;
     }
 
     if (input.isKeyPressed(ControlKeys::Up)) {
-        cameraWorldPosition.y += movementSpeed;
+        cameraWorldPosition.y += keyboardMovementSpeed;
     }
 
     if (input.isKeyPressed(ControlKeys::Down)) {
-        cameraWorldPosition.y -= movementSpeed;
+        cameraWorldPosition.y -= keyboardMovementSpeed;
     }
 
-    //Handle camera pan/tilt with mouse controls here
-    calculateRotation();
+    updateCameraRotationAngles(input.getMouseState());
+
+    auto target = glm::normalize(glm::rotateY(forwardVector, horizontalAngle));
+    auto horizontalAxis = glm::normalize(glm::cross(glm::vec3(0.0f, 1.0f, 0.0f), target));
+    target = glm::normalize(glm::rotateX(forwardVector, verticalAngle));
+    auto verticalAxis = glm::normalize(glm::cross(horizontalAxis, target));
 
     viewMatrix = glm::lookAt(
         cameraWorldPosition,
-        (cameraDirection + cameraWorldPosition) * cameraRotation,
-        glm::vec3(0, 1, 0)
+        // (cameraDirection + cameraWorldPosition) * cameraRotation,
+        (target + cameraWorldPosition),
+        // glm::vec3(0, 1, 0)
+        verticalAxis
     );
 }
